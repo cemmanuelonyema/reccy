4fb6a691a9afe42365cb563407e0fde7e8e7cf23
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-01-02 06:18:47 +0100

    feat: add recipeItem component

---
+++ src/components/molecules/recipeItem/RecipeItem.jsx
@@ -0,0 +1,8 @@
+import React from 'react'
+import styles from './RecipeItem.module.scss';
+
+export const RecipeItem = () => {
+  return (
+    <div>RecipeItem</div>
+  )
+}
--- src/components/pages/recipes/Recipes.jsx
+++ src/components/pages/recipes/Recipes.jsx
@@ -1,8 +1,14 @@
-import React from 'react'
+import React from 'react';
+import { RecipeItem } from '../../molecules/recipeItem/RecipeItem';
 import styles from './Recipes.module.scss';
 
 export const Recipes = () => {
-  return (
-    <div>Recipes</div>
-  )
-}
+      const datas = [1, 2, 3, 4, 5, 67, 8, 9, 0];
+      return (
+            <div>
+                  {datas.map((x, i) => (
+                        <RecipeItem key={i} />
+                  ))}
+            </div>
+      );
+};
