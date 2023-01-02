8cce7ce7d2ef67ec349787d8b0042dffa70224b4
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-01-02 12:11:50 +0100

    feat: update recipe item component

--- src/components/molecules/recipeItem/RecipeItem.jsx
+++ src/components/molecules/recipeItem/RecipeItem.jsx
@@ -1,8 +1,23 @@
+//dependency import
 import React from 'react'
+import { Link } from 'react-router-dom';
+//files import
 import styles from './RecipeItem.module.scss';
 
-export const RecipeItem = () => {
-  return (
-    <div>RecipeItem</div>
-  )
+export const RecipeItem = ({recipe}) => {
+   return (
+            <Link to={`/recipes/${recipe?.id}`}>
+                  <article className={styles.recipe}>
+                        <div className={styles.recipe__imgBox}>
+                              <img src={recipe?.image_url} alt="recipe image" className={styles.recipe__img}/>
+                        </div>
+                        <div className={styles.recipe__content}>
+                              <span className={styles.recipe__title}>{recipe?.title}</span>
+                              <span className={styles.recipe__publisher}>
+                                    Publisher: {recipe?.publisher}
+                              </span>
+                        </div>
+                  </article>
+            </Link>
+      );
 }
