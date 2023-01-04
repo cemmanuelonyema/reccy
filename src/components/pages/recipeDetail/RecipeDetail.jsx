834d8eb06465daf934d597ab22aa1ead9d4264db
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-01-04 14:12:22 +0100

    refactor: delete unused codes pagination implementation code

--- src/components/pages/recipeDetail/RecipeDetail.jsx
+++ src/components/pages/recipeDetail/RecipeDetail.jsx
@@ -168,55 +168,64 @@ export const RecipeDetail = () => {
                                     <use href="src/img/icons.svg#icon-bookmark-fill"></use>
                               </svg>
                         </button>
-                  <div class="recipe__ingredients">
-                        <h2 class="heading--2">Recipe ingredients</h2>
-                        <ul class="recipe__ingredient-list">
-                              <li class="recipe__ingredient">
-                                    <svg class="recipe__icon">
-                                          <use href="src/img/icons.svg#icon-check"></use>
+                        <div class="recipe__ingredients">
+                              <h2 class="heading--2">Recipe ingredients</h2>
+                              <ul class="recipe__ingredient-list">
+                                    <li class="recipe__ingredient">
+                                          <svg class="recipe__icon">
+                                                <use href="src/img/icons.svg#icon-check"></use>
+                                          </svg>
+                                          <div class="recipe__quantity">
+                                                1000
+                                          </div>
+                                          <div class="recipe__description">
+                                                <span class="recipe__unit">
+                                                      g
+                                                </span>
+                                                pasta
+                                          </div>
+                                    </li>
+
+                                    <li class="recipe__ingredient">
+                                          <svg class="recipe__icon">
+                                                <use href="src/img/icons.svg#icon-check"></use>
+                                          </svg>
+                                          <div class="recipe__quantity">
+                                                0.5
+                                          </div>
+                                          <div class="recipe__description">
+                                                <span class="recipe__unit">
+                                                      cup
+                                                </span>
+                                                ricotta cheese
+                                          </div>
+                                    </li>
+                              </ul>
+                        </div>
+
+                        <div class="recipe__directions">
+                              <h2 class="heading--2">How to cook it</h2>
+                              <p class="recipe__directions-text">
+                                    This recipe was carefully designed and
+                                    tested by
+                                    <span class="recipe__publisher">
+                                          The Pioneer Woman
+                                    </span>
+                                    . Please check out directions at their
+                                    website.
+                              </p>
+                              <a
+                                    class="btn--small recipe__btn"
+                                    href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
+                                    target="_blank"
+                              >
+                                    <span>Directions</span>
+                                    <svg class="search__icon">
+                                          <use href="src/img/icons.svg#icon-arrow-right"></use>
                                     </svg>
-                                    <div class="recipe__quantity">1000</div>
-                                    <div class="recipe__description">
-                                          <span class="recipe__unit">g</span>
-                                          pasta
-                                    </div>
-                              </li>
-
-                              <li class="recipe__ingredient">
-                                    <svg class="recipe__icon">
-                                          <use href="src/img/icons.svg#icon-check"></use>
-                                    </svg>
-                                    <div class="recipe__quantity">0.5</div>
-                                    <div class="recipe__description">
-                                          <span class="recipe__unit">cup</span>
-                                          ricotta cheese
-                                    </div>
-                              </li>
-                        </ul>
+                              </a>
+                        </div>
                   </div>
-
-                  <div class="recipe__directions">
-                        <h2 class="heading--2">How to cook it</h2>
-                        <p class="recipe__directions-text">
-                              This recipe was carefully designed and tested by
-                              <span class="recipe__publisher">
-                                    The Pioneer Woman
-                              </span>
-                              . Please check out directions at their website.
-                        </p>
-                        <a
-                              class="btn--small recipe__btn"
-                              href="http://thepioneerwoman.com/cooking/pasta-with-tomato-cream-sauce/"
-                              target="_blank"
-                        >
-                              <span>Directions</span>
-                              <svg class="search__icon">
-                                    <use href="src/img/icons.svg#icon-arrow-right"></use>
-                              </svg>
-                        </a>
-                  </div>
-                  </div>
-
             </section>
       );
 };
