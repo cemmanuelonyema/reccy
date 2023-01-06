c0bbf68cd691ee13b1020481bc231c58db9f0db6
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-01-06 18:34:47 +0100

    feat: update footer component and add to recipe detail

--- src/components/shared/footer/Footer.jsx
+++ src/components/shared/footer/Footer.jsx
@@ -1,10 +1,21 @@
-import React from 'react'
+import React from 'react';
 import styles from './Footer.module.scss';
 
 export const Footer = () => {
-  return (
-    <footer>
-        <div className='container'>footer</div>
-    </footer>
-  )
-}
+      const date = new Date();
+      return (
+            <footer className={styles.footer}>
+                  <div className={styles.footer__container}>
+                        <span>
+                              {date.getFullYear()} Reccy - Developed by
+                              <a
+                                    href="https://ceo-ox.vecel.app"
+                                    target="_blank"
+                              >
+                                    Emmanuel Onyema
+                              </a>
+                        </span>
+                  </div>
+            </footer>
+      );
+};
