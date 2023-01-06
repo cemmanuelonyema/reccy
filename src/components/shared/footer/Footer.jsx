bbed87a4a003b40ec4c175c2ed5e865020e948ce
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-01-06 16:33:01 +0100

    feat: add footer component

---
+++ src/components/shared/footer/Footer.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import styles from './Footer.module.scss';
+
+export const Footer = () => {
+  return (
+    <footer>
+        <div className='container'>footer</div>
+    </footer>
+  )
+}
