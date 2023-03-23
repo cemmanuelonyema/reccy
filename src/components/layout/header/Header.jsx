531b7c7a8b1139a1b4269e680d4c7872d3b6cbe9
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-03-23 20:35:00 +0100

    refactor: add nav

--- src/components/layout/header/Header.jsx
+++ src/components/layout/header/Header.jsx
@@ -1,12 +1,15 @@
 import {} from 'react';
 import styles from './Header.module.scss';
+import Nav from '../nav/Nav';
+import { SearchForm } from '../../molecules/searchForm/SearchForm';
 
 const Header = () => {
       return (
             <header className={styles.header}>
                   <div className={`${styles.header__container} container`}>
-                        <img src="" alt="" />
-                        {/* search bar for big screen */}
+                        <img src="" alt="logo" />
+                        <SearchForm />
+                        <Nav />
                   </div>
             </header>
       );
