6a358b34bfd5f97f73574498ce98c4dc24e96f3b
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-04-12 19:05:11 +0100

    feat: add landing sections

--- src/components/pages/landing/Landing.jsx
+++ src/components/pages/landing/Landing.jsx
@@ -1,15 +1,29 @@
 import React from 'react';
 import styles from './Landing.module.scss';
 import { Link } from 'react-router-dom';
+import Hero from './components/hero/Hero';
+import Featured from './components/featured/Featured';
+import Meals from './components/meals/Meals';
+import Testimonial from './components/testimonial/Testimonial';
+import { Gallery } from './components/gallery/Gallery';
+import Features from './components/features/Features';
+import Pricing from './components/pricing/Pricing';
+import Contact from './components/contact/Contact';
+import Steps from './components/steps/Steps';
 
 const Landing = () => {
       return (
-            <div>
-                  Landing
-                  <nav>
-                        <Link to="/recipes">search recipes</Link>
-                  </nav>
-            </div>
+            <>
+                  <Hero />
+                  <Featured />
+                  <Features />
+                  <Steps />
+                  <Meals />
+                  <Testimonial />
+                  <Gallery />
+                  <Pricing />
+                  <Contact />
+            </>
       );
 };
 
