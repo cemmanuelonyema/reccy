e9025e124e7431bc8714bc3b62d7f37727b4235d
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-04-15 23:34:44 +0100

    refactor: add recipe linking

--- src/components/pages/landing/components/hero/Hero.jsx
+++ src/components/pages/landing/components/hero/Hero.jsx
@@ -1,8 +1,14 @@
 import React from 'react';
 import styles from './Hero.module.scss';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
-      return <section>Hero</section>;
+      return (
+            <section>
+                  Hero
+                  <Link to="/recipes"> Search Recipe</Link>
+            </section>
+      );
 };
 
 export default Hero;
