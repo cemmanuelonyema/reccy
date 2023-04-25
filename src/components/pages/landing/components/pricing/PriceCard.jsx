8a4d29afc1dbb251a1b54bfba0773b2555f4019c
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-04-25 19:10:54 +0100

    feat: add prices and price card comp markup

---
+++ src/components/pages/landing/components/pricing/PriceCard.jsx
@@ -0,0 +1,38 @@
+import styles from './PriceCard.module.scss';
+
+export const Card = ({ data: { id, price, title, items } }) => (
+      <article className={styles.card}>
+            <div className="card__side card__side--front">
+                  <div className={`card__picture card__picture--${id}`}>
+                        &nbsp;
+                  </div>
+                  <h4 className="card__heading">
+                        <span
+                              className={`card__heading-span card__heading-span--${id}`}
+                        >
+                              {title}
+                        </span>
+                  </h4>
+                  <div className="card__details">
+                        <ul>
+                              {items?.map((item, i) => (
+                                    <li key={i}>{item}</li>
+                              ))}
+                        </ul>
+                  </div>
+            </div>
+            <div
+                  className={`card__side card__side--back card__side--back-${id}`}
+            >
+                  <div className="card__cta">
+                        <div className="card__price-box">
+                              <p className="card__price-only">Only</p>
+                              <p className="card__price-value">{price}</p>
+                        </div>
+                        <a href="#popup" className="btn btn--white">
+                              Book now!
+                        </a>
+                  </div>
+            </div>
+      </article>
+);
