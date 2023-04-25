7a207ec63b44890b75edf4b89d6969a0e17ca907
Emmanuel C. Onyema <cemmanuelonyema@gmail.com>
2023-04-26 00:42:34 +0100

    Price (#32)
    * refactor: add cta form
    
    * refactor: update contact component
    
    * refactor: add hero component
    
    * refactor: modify recipeDetail list
    
    * feat: add features markup
    
    * feat: add steps markup
    
    * feat: add prices and price card comp markup

--- src/components/pages/landing/components/pricing/Pricing.jsx
+++ src/components/pages/landing/components/pricing/Pricing.jsx
@@ -1,8 +1,66 @@
 import React from 'react';
 import styles from './Pricing.module.scss';
+import { Card } from './PriceCard';
+
+const pricesData = [
+      {
+            id: 1,
+            title: 'lorem 1',
+            img: 'img 1',
+            price: '$ 457',
+            items: [
+                  'lorem data 1',
+                  'lorem data 1',
+                  'lorem data 1',
+                  'lorem data 1',
+            ],
+      },
+      {
+            id: 2,
+            title: 'lorem 2',
+            img: 'img 2',
+            price: '$ 457',
+            items: [
+                  'lorem data 2',
+                  'lorem data 2',
+                  'lorem data 2',
+                  'lorem data 2',
+            ],
+      },
+      {
+            id: 3,
+            title: 'lorem 3',
+            img: 'img 3',
+            price: '$ 457',
+            items: [
+                  'lorem data 3',
+                  'lorem data 3',
+                  'lorem data 3',
+                  'lorem data 3',
+            ],
+      },
+];
 
 const Pricing = () => {
-      return <section>Pricing</section>;
+      return (
+            <section>
+                  <div className={`${styles.footer__container} container`}>
+                        <div>
+                              <h2>Pricing</h2>
+                              <p>
+                                    Lorem ipsum dolor sit amet consectetur
+                                    adipisicing elit. Iure sapiente in tempore!
+                                    Neque, eveniet optio!
+                              </p>
+                        </div>
+                        <div className={styles.prices}>
+                              {pricesData.map((item) => (
+                                    <Card key={item.id} data={item} />
+                              ))}
+                        </div>
+                  </div>
+            </section>
+      );
 };
 
 export default Pricing;
