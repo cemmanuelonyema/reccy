a35102edbc8fdb785aae0e1ca5b4e5552742ba7b
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-04-27 23:16:30 +0100

    refactor: update price card

--- src/components/pages/landing/components/pricing/PriceCard.jsx
+++ src/components/pages/landing/components/pricing/PriceCard.jsx
@@ -2,18 +2,20 @@ import styles from './PriceCard.module.scss';
 
 export const Card = ({ data: { id, price, title, items } }) => (
       <article className={styles.card}>
-            <div className="card__side card__side--front">
-                  <div className={`card__picture card__picture--${id}`}>
+            <div className={`${styles.card__side} ${styles.card__side__front}`}>
+                  <div
+                        className={`${styles.card__picture} ${styles.card__picture__}`}
+                  >
                         &nbsp;
                   </div>
-                  <h4 className="card__heading">
+                  <h4 className={styles.card__heading}>
                         <span
-                              className={`card__heading-span card__heading-span--${id}`}
+                              className={`${styles.card__heading_span} ${styles.card__heading_span__}`}
                         >
                               {title}
                         </span>
                   </h4>
-                  <div className="card__details">
+                  <div className={styles.card__details}>
                         <ul>
                               {items?.map((item, i) => (
                                     <li key={i}>{item}</li>
@@ -22,12 +24,15 @@ export const Card = ({ data: { id, price, title, items } }) => (
                   </div>
             </div>
             <div
-                  className={`card__side card__side--back card__side--back-${id}`}
+                  //   className={`card__side card__side--back card__side--back-${id}`}
+                  className={`${styles.card__side} ${styles.card__side__back} ${styles.card__side__back_}`}
             >
-                  <div className="card__cta">
-                        <div className="card__price-box">
-                              <p className="card__price-only">Only</p>
-                              <p className="card__price-value">{price}</p>
+                  <div className={styles.card__cta}>
+                        <div className={styles.card__price_box}>
+                              <p className={styles.card__price_only}>Only</p>
+                              <p className={styles.card__price_value}>
+                                    {price}
+                              </p>
                         </div>
                         <a href="#popup" className="btn btn--white">
                               Book now!
