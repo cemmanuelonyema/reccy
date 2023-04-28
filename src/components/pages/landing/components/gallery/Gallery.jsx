168abd9bb0d7b7d7d6ddfcf512e0d20c4d2ae220
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-04-28 05:21:55 +0100

    refactor: add testimony gallery section

--- src/components/pages/landing/components/gallery/Gallery.jsx
+++ src/components/pages/landing/components/gallery/Gallery.jsx
@@ -2,5 +2,95 @@ import React from 'react';
 import styles from './Gallery.module.scss';
 
 export const Gallery = () => {
-      return <section>Gallery</section>;
+      return (
+            <section>
+                  <div class="gallery">
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-1.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                              {/* <!-- <figcaption>Caption</figcaption> --> */}
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-2.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-3.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-4.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-5.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-6.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-7.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-8.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-9.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-10.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-11.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                        <figure class="gallery-item">
+                              <img
+                                    src="img/gallery/gallery-12.jpg"
+                                    alt="Photo of beautifully
+            arranged food"
+                              />
+                        </figure>
+                  </div>
+            </section>
+      );
 };
