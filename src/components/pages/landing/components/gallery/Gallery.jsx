fd6fd3273d3b6276aa265acaa0c3dda0727b74d1
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-04-29 12:21:09 +0100

    refactor: update gallery component

--- src/components/pages/landing/components/gallery/Gallery.jsx
+++ src/components/pages/landing/components/gallery/Gallery.jsx
@@ -1,95 +1,34 @@
 import React from 'react';
 import styles from './Gallery.module.scss';
+import img1 from '/img/customer-1.jpg';
+
+const galleryimages = [
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+      { src: img1 },
+];
 
 export const Gallery = () => {
       return (
             <section>
-                  <div class="gallery">
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-1.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                              {/* <!-- <figcaption>Caption</figcaption> --> */}
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-2.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-3.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-4.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-5.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-6.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-7.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-8.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-9.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-10.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-11.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
-                        <figure class="gallery-item">
-                              <img
-                                    src="img/gallery/gallery-12.jpg"
-                                    alt="Photo of beautifully
-            arranged food"
-                              />
-                        </figure>
+                  <div class={styles.gallery}>
+                        {galleryimages.map(() => (
+                              <figure class={styles.gallery_item}>
+                                    <img
+                                          src={img1}
+                                          alt="Photo of a food in our gallery"
+                                    />
+                              </figure>
+                        ))}
                   </div>
             </section>
       );
