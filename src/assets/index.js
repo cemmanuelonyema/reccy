2e67fa68c5ab7c3f1d434872a1bc938e41293cf1
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-05-10 11:38:11 +0100

    refactor: refactor assets import

--- src/assets/index.js
+++ src/assets/index.js
@@ -3,5 +3,38 @@ import { FaPlus, FaBookmark, FaHamburger, FaTimes } from 'react-icons/fa';
 
 //images
 import logo from './img/logo.png';
+import featured1 from './img/business-insider.png';
+import featured2 from './img/forbes.png';
+import featured3 from './img/techcrunch.png';
+import featured4 from './img/the-new-york-times.png';
+import featured5 from './img/usa-today.png';
+import heroDish from './img/hero-dish.png';
+import dish1 from './img/customer-1.jpg';
 
-export { logo, FaPlus, FaBookmark, FaHamburger, FaTimes };
+import cus1 from './img/customer-1.jpg';
+import cus2 from './img/customer-2.jpg';
+import cus3 from './img/customer-3.jpg';
+import cus4 from './img/customer-4.jpg';
+import cus5 from './img/customer-5.jpg';
+import cus6 from './img/customer-6.jpg';
+
+export {
+      logo,
+      featured1,
+      featured2,
+      featured3,
+      featured4,
+      featured5,
+      heroDish,
+      dish1,
+      cus1,
+      cus2,
+      cus3,
+      cus4,
+      cus5,
+      cus6,
+      FaPlus,
+      FaBookmark,
+      FaHamburger,
+      FaTimes,
+};
