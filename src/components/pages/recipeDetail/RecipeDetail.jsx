142de6e64efd195b7ceac45042bfd38362e2d7f1
cemmanuelonyema <cemmanuelonyema@gmail.com>
2023-05-24 01:46:40 +0100

    refactor: fix

--- src/components/pages/recipeDetail/RecipeDetail.jsx
+++ src/components/pages/recipeDetail/RecipeDetail.jsx
@@ -8,57 +8,56 @@ import { Footer } from '../../shared/footer/Footer';
 //style
 import styles from './RecipeDetail.module.scss';
 
-const recipe = {
-      ingredients: [
-            {
-                  quantity: 1,
-                  unit: '',
-                  description: 'tbsp. canola or olive oil',
-            },
-            {
-                  quantity: 0.5,
-                  unit: 'cup',
-                  description: 'chopped sweet onion',
-            },
-            {
-                  quantity: 3,
-                  unit: 'cups',
-                  description: 'diced fresh red yellow and green bell peppers',
-            },
-            {
-                  quantity: 1,
-                  unit: '',
-                  description: 'tube refrigerated pizza dough',
-            },
-            {
-                  quantity: 0.5,
-                  unit: 'cup',
-                  description: 'salsa',
-            },
-            {
-                  quantity: 2,
-                  unit: 'cups',
-                  description: 'sargento chefstyle shredded pepper jack cheese',
-            },
-            {
-                  quantity: null,
-                  unit: '',
-                  description: 'Chopped cilantro or dried oregano',
-            },
-      ],
-      source_url:
-            'http://www.mybakingaddiction.com/spicy-chicken-and-pepper-jack-pizza-recipe/',
-      image_url:
-            'http://forkify-api.herokuapp.com/images/FlatBread21of1a180.jpg',
-      title: 'Spicy Chicken and Pepper Jack Pizza',
-      servings: 4,
-      cooking_time: 45,
-};
+// const recipe = {
+//       ingredients: [
+//             {
+//                   quantity: 1,
+//                   unit: '',
+//                   description: 'tbsp. canola or olive oil',
+//             },
+//             {
+//                   quantity: 0.5,
+//                   unit: 'cup',
+//                   description: 'chopped sweet onion',
+//             },
+//             {
+//                   quantity: 3,
+//                   unit: 'cups',
+//                   description: 'diced fresh red yellow and green bell peppers',
+//             },
+//             {
+//                   quantity: 1,
+//                   unit: '',
+//                   description: 'tube refrigerated pizza dough',
+//             },
+//             {
+//                   quantity: 0.5,
+//                   unit: 'cup',
+//                   description: 'salsa',
+//             },
+//             {
+//                   quantity: 2,
+//                   unit: 'cups',
+//                   description: 'sargento chefstyle shredded pepper jack cheese',
+//             },
+//             {
+//                   quantity: null,
+//                   unit: '',
+//                   description: 'Chopped cilantro or dried oregano',
+//             },
+//       ],
+//       source_url:
+//             'http://www.mybakingaddiction.com/spicy-chicken-and-pepper-jack-pizza-recipe/',
+//       image_url:
+//             'http://forkify-api.herokuapp.com/images/FlatBread21of1a180.jpg',
+//       title: 'Spicy Chicken and Pepper Jack Pizza',
+//       servings: 4,
+//       cooking_time: 45,
+// };
 
 export const RecipeDetail = () => {
       //hooks
-      const { getRecipe } = useContext(RecipeContext);
-      //   const { getRecipe, recipe } = useContext(RecipeContext);
+      const { getRecipe, recipe } = useContext(RecipeContext);
       console.log(recipe);
       const { id } = useParams();
       //useEffect
@@ -133,50 +132,6 @@ export const RecipeDetail = () => {
                         <div class={styles.recipeDetail__ingredients}>
                               <h2 class="heading--2">Recipe ingredients</h2>
                               <ul class={styles.recipeDetail__ingredientList}>
-                                    {/* <li class={styles.recipeDetail__ingredient}>
-                                          <Icon iconName="check" />
-
-                                          <div
-                                                class={
-                                                      styles.recipeDetail__quantity
-                                                }
-                                          >
-                                                1000
-                                          </div>
-                                          <div
-                                                class={
-                                                      styles.recipeDetail__description
-                                                }
-                                          >
-                                                <span class="recipeDetail__unit">
-                                                      g
-                                                </span>
-                                                pasta
-                                          </div>
-                                    </li>
-
-                                    <li class={styles.recipeDetail__ingredient}>
-                                          <Icon iconName="check" />
-
-                                          <div
-                                                class={
-                                                      styles.recipeDetail__quantity
-                                                }
-                                          >
-                                                0.5
-                                          </div>
-                                          <div
-                                                class={
-                                                      styles.recipeDetail__description
-                                                }
-                                          >
-                                                <span class="recipe__unit">
-                                                      cup
-                                                </span>
-                                                ricotta cheese
-                                          </div>
-                                    </li> */}
-
                                     {recipe?.ingredients?.map((item) => (
                                           <li
                                                 class={
@@ -187,15 +142,6 @@ export const RecipeDetail = () => {
                                                       size={24}
                                                       color="#55c57a"
                                                 />
-                                                {/* <div className={styles.ingredient_box}>
-                                                      <span>
-                                                            {item.quantity}
-                                                      </span>
-                                                      <span> {item.unit}</span>
-                                                      <span>
-                                                            {item.description}
-                                                      </span>
-                                                </div> */}
 
                                                 <div
                                                       class={
